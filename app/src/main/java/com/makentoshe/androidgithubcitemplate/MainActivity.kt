7898949da9d236f985379b30370ffe877229d4da
bialger <artur.bigulov@yandex.ru>
2021-12-18 15:33:25 +0300

    Used SoundPool (example!); fixed incorrect displaying of UI elements; NOT fixed incorrect displaying of play, pause etc. buttons

--- app/src/main/java/com/makentoshe/androidgithubcitemplate/MainActivity.kt
+++ app/src/main/java/com/makentoshe/androidgithubcitemplate/MainActivity.kt
@@ -3,19 +3,39 @@ package com.makentoshe.androidgithubcitemplate
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.R
-
+import android.content.pm.ActivityInfo
+import android.media.AudioManager
+import android.media.SoundPool
 import android.media.MediaPlayer
 import android.util.Log
 import android.view.View
+import android.widget.Toast
 import android.widget.Button
 import androidx.fragment.app.FragmentActivity
+import java.util.concurrent.TimeUnit
 
 
+@Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
-
+    private var soundPool: SoundPool? = null
+    private var soundId = 1
+    private var soundId2 = 1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(com.makentoshe.androidgithubcitemplate.R.layout.activity_main)
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        soundPool = SoundPool(1, AudioManager.STREAM_MUSIC, 0)
+        soundId = soundPool!!.load(baseContext, com.makentoshe.androidgithubcitemplate.R.raw.yf___vinnyx__crash_, 0)
+        soundId2 = soundPool!!.load(baseContext, com.makentoshe.androidgithubcitemplate.R.raw.yf___vinnyx__crash_, 0)
+    }
+    fun playSound(view: View) { // TODO: заменить onClick у KICK с этого на правильный, когда будет исправлена раскладка
+        Toast.makeText(this, "Playing compilated music...", Toast.LENGTH_SHORT).show()
+        var ratio = 0.5F // по фану, для демонстрации
+        soundPool?.play(soundId, 0.5F, 0.5F, 0, 0, ratio)
+        var soundLen : Long = 4 // TODO: заменить на подсчет длины звука
+        TimeUnit.SECONDS.sleep((soundLen / ratio).toLong())
+        soundPool?.play(soundId2, 1F, 1F, 0, 0, 1F)
+        Toast.makeText(this, "Completed!", Toast.LENGTH_SHORT).show()
     }
 }
 //321123
