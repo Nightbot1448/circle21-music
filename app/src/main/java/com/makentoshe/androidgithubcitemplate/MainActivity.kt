06cb7ee4024b1930520605b17926b1b322685916
Artem <artem.s.grigorash@gmail.com>
2022-02-19 20:48:26 +0300

    time bar and menu

--- app/src/main/java/com/makentoshe/androidgithubcitemplate/MainActivity.kt
+++ app/src/main/java/com/makentoshe/androidgithubcitemplate/MainActivity.kt
@@ -1,8 +1,8 @@
 package com.makentoshe.androidgithubcitemplate
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.content.Intent
 import android.content.pm.ActivityInfo
 import android.media.AudioManager
 import android.media.SoundPool
@@ -11,11 +11,13 @@ import android.util.Log
 import android.view.View
 import android.widget.Toast
 import android.widget.Button
-import android.widget.TextView
 import androidx.fragment.app.FragmentActivity
-import java.io.File
-import java.io.FileOutputStream
 import java.util.concurrent.TimeUnit
+import android.view.ContextMenu.ContextMenuInfo
+
+import android.view.ContextMenu
+import android.widget.TextView
+import androidx.navigation.findNavController
 
 
 @Suppress("DEPRECATION")
@@ -23,25 +25,56 @@ class MainActivity : AppCompatActivity() {
     private var soundPool: SoundPool? = null
     private var soundId = 1
     private var soundId2 = 1
-    override fun onStart() {
-        super.onStart()
-        val intent = Intent(this, HelpActivity::class.java)
-        findViewById<TextView>(com.makentoshe.androidgithubcitemplate.R.id.help).setOnClickListener {
-            startActivity(intent)
-        }
-        val intent1 = Intent(this, AddingfilesActivity::class.java)
-        findViewById<TextView>(com.makentoshe.androidgithubcitemplate.R.id.file).setOnClickListener {
-            startActivity(intent1)
-        }
-    }
+    private var soundId3 = 1
+    private var soundId4 = 1
+    private var menu1 = 1
+    private var menu2 = 2
+    private var menu3 = 3
+    //опишем создание контекстных меню
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         soundPool = SoundPool(1, AudioManager.STREAM_MUSIC, 0)
-        soundId = soundPool!!.load(baseContext, R.raw.file1, 0)
-        soundId2 = soundPool!!.load(baseContext, R.raw.file1, 0)
+        soundId = soundPool!!.load(baseContext, com.makentoshe.androidgithubcitemplate.R.raw.file1, 0)
+        soundId2 = soundPool!!.load(baseContext, com.makentoshe.androidgithubcitemplate.R.raw.file1, 0)
+        soundId3 = soundPool!!.load(baseContext, com.makentoshe.androidgithubcitemplate.R.raw.file2, 0)
+        soundId4 = soundPool!!.load(baseContext, com.makentoshe.androidgithubcitemplate.R.raw.file2, 0)
+
+        var firstsound = findViewById<Button>(R.id.KICK)
+
+        //создание контекстного меню
+        registerForContextMenu(firstsound);
     }
+// ниже создание переходов между экранами
+    override fun onStart() {
+        super.onStart()
+        val intent = Intent(this, HelpActivity::class.java)
+        findViewById<TextView>(R.id.help).setOnClickListener {
+            startActivity(intent)
+        }
+        val intent1 = Intent(this, AddingfilesActivity::class.java)
+        findViewById<TextView>(R.id.file).setOnClickListener {
+            startActivity(intent1)
+            }
+    }
+
+    //findNavController(R.id.app_graph).navigate(R.id.action_mainActivity_to_helpActivity)
+    override fun onCreateContextMenu(
+        menu: ContextMenu, v: View,
+        menuInfo: ContextMenuInfo?
+    ) {
+        // TODO Auto-generated method stub
+        when (v.id) {
+            R.id.KICK -> {
+                menu.add(0, menu1, 0, "kick")
+                menu.add(0, menu2, 0, "snare")
+                menu.add(0, menu3, 0, "hihat")
+            }
+
+        }
+    }
+
     fun playSound(view: View) { // TODO: заменить onClick у KICK с этого на правильный, когда будет исправлена раскладка
         Toast.makeText(this, "Playing compilated music...", Toast.LENGTH_SHORT).show()
         var ratio = 0.5F // по фану, для демонстрации
@@ -51,43 +84,16 @@ class MainActivity : AppCompatActivity() {
         soundPool?.play(soundId2, 1F, 1F, 0, 0, 1F)
         Toast.makeText(this, "Completed!", Toast.LENGTH_SHORT).show()
     }
+    fun playSound1(view: View) { // TODO: заменить onClick у KICK с этого на правильный, когда будет исправлена раскладка
+        Toast.makeText(this, "Playing compilated music...", Toast.LENGTH_SHORT).show()
+        var ratio = 0.5F // по фану, для демонстрации
+        soundPool?.play(soundId3, 0.5F, 0.5F, 0, 0, ratio)
+        var soundLen1 : Long = 2 // TODO: заменить на подсчет длины звука
+        TimeUnit.SECONDS.sleep((soundLen1 / ratio).toLong())
+        soundPool?.play(soundId4, 1F, 1F, 0, 0, 1F)
+        Toast.makeText(this, "Completed 22!", Toast.LENGTH_SHORT).show()
+    }
+
 }
 
-@Suppress("DEPRECATION")
-class HelpActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_help)
-    }
-    override fun onStart() {
-        super.onStart()
-        val intent = Intent(this, MainActivity::class.java)
-        findViewById<TextView>(R.id.help).setOnClickListener {
-            startActivity(intent)
-        }
-    }
-}
 
-class AddingfilesActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_addfiles)
-    }
-    override fun onStart() {
-        super.onStart()
-        val intent = Intent(this, MainActivity::class.java)
-        findViewById<TextView>(R.id.file).setOnClickListener {
-            startActivity(intent)
-        }
-    }
-    fun openFile(view: View) {
-        Toast.makeText(this, "Opening file...", Toast.LENGTH_SHORT).show()
-        val path = getFilesDir()
-        val file = File(path, "file.txt")
-        FileOutputStream(file).use {
-            it.write("record goes here".toByteArray())
-        }
-        val contents = file.readText()
-        Toast.makeText(this, contents, Toast.LENGTH_SHORT).show()
-    }
-}
